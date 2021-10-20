ed964950d7cd146b269767d7505496c1daf21ace
Mark John Balcueva <75126648+mjbalcueva@users.noreply.github.com>
2021-10-21 04:13:56 +0800

    add basic ui colors

--- src/theme.js
+++ src/theme.js
@@ -13,7 +13,40 @@ const getTheme = ({ theme, name }) => {
 	return {
 		name: name,
 		//~ UI Colors
-		colors: {},
+		colors: {
+			foreground: hex(color.fg.default),
+			'icon.foreground': hex(color.fg.default),
+			'sash.hoverBorder': hex(color.accent.emphasis),
+
+			'progressBar.background': hex(color.accent.emphasis),
+
+			'activityBar.background': hex(color.canvas.tertiary),
+			'activityBar.foreground': hex(color.fg.default),
+
+			'sideBar.background': hex(color.canvas.secondary),
+			'sideBarSectionHeader.background': hex(color.canvas.tertiary),
+
+			'editor.background': hex(color.canvas.primary),
+
+			'editorGroupHeader.tabsBackground': hex(color.canvas.secondary),
+			'editorGroupHeader.noTabsBackground': hex(color.canvas.secondary),
+
+			'editorWidget.background': hex(color.canvas.secondary),
+			'editorWidget.foreground': hex(color.fg.default),
+
+			'panel.background': hex(color.canvas.secondary),
+			'panelTitle.activeForeground': hex(color.fg.default),
+
+			'statusBar.background': hex(color.canvas.tertiary),
+			'statusBar.foreground': hex(color.fg.default),
+
+			'tab.inactiveBackground': hex(color.canvas.secondary),
+
+			'titleBar.activeBackground': hex(color.canvas.tertiary),
+			'titleBar.inactiveBackground': hex(color.canvas.tertiary),
+			'titleBar.activeForeground': hex(color.fg.default),
+			'titleBar.inactiveForeground': hex(color.fg.default),
+		},
 		//~ Semantic Sytax Highlighting
 		semanticHighlighting: true,
 		semanticTokenColors: {},
