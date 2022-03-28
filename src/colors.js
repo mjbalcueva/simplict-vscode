98000267fe02200dc894444bdadbcb638b4f8a2d
Mark John Balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-03-28 18:21:55 +0800

    Cleaned workspace

--- src/colors.js
+++ src/colors.js
@@ -1,17 +1,24 @@
-const darkColors = require('./themeColors/dark.json');
-const dimmedColors = require('./themeColors/dimmed.json');
-const lightColors = require('./themeColors/light.json');
+const darkColors = require('./themeColors/dark.json')
+
+/**
+ *	const dimmedColors = require('./themeColors/dimmed.json')
+ *	const lightColors = require('./themeColors/light.json')
+ *
+ *	function getColors(theme) {
+ *		if (theme === 'dark') {
+ *			return darkColors
+ *		} else if (theme === 'dimmed') {
+ *			return dimmedColors
+ *		} else if (theme === 'light') {
+ *			return lightColors
+ *		}
+ *	}
+ */
 
 function getColors(theme) {
-	if (theme === 'dark') {
-		return darkColors;
-	} else if (theme === 'dimmed') {
-		return dimmedColors;
-	} else if (theme === 'light') {
-		return lightColors;
-	}
+	return darkColors
 }
 
 module.exports = {
 	getColors,
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
-const fs = require('fs').promises;
-const getTheme = require('./theme');
+const fs = require('fs').promises
+const getTheme = require('./theme')
 
 const darkDefaultTheme = getTheme({
 	theme: 'dark',
 	name: 'SimpliCT Dark Default',
-});
+})
 
-// const darkDimmedTheme = getTheme({
-// 	theme: 'dimmed',
-// 	name: 'SimpliCT Dark Dimmed',
-// });
-
-// const lightDefaultTheme = getTheme({
-// 	theme: 'light',
-// 	name: 'SimpliCT Light Default',
-// });
+/**
+ *	const darkDimmedTheme = getTheme({
+ *		theme: 'dimmed',
+ *		name: 'SimpliCT Dark Dimmed',
+ *	});
+ *
+ *	const lightDefaultTheme = getTheme({
+ *		theme: 'light',
+ *		name: 'SimpliCT Light Default',
+ *	});
+ */
 
 fs.mkdir('./themes', { recursive: true })
-	.then(() => Promise.all([
-			fs.writeFile( './themes/dark-default.json',
-				JSON.stringify(darkDefaultTheme, null, 2),
-			),
-			// fs.writeFile( './themes/dark-dimmed.json',
-			// 	JSON.stringify(darkDimmedTheme, null, 2),
-			// ),
-			// fs.writeFile( './themes/light-default.json',
-			// 	JSON.stringify(lightDefaultTheme, null, 2),
-			// ),
+	.then(() =>
+		Promise.all([
+			fs.writeFile('./themes/dark-default.json', JSON.stringify(darkDefaultTheme, null, 2)),
+			/**
+			 *	fs.writeFile(
+			 *		'./themes/dark-dimmed.json',
+			 *		JSON.stringify(darkDimmedTheme, null, 2),
+			 *	),
+			 *
+			 *	fs.writeFile(
+			 *		'./themes/light-default.json',
+			 *		JSON.stringify(lightDefaultTheme, null, 2),
+			 *	),
+			 */
 		]),
 	)
-	.catch(() => process.exit(1));
+	.catch(() => process.exit(1))
